ffb8ac7588dc835e93228647f6bb608c7f7aa17c
Mark Smith <smith.h.mark@gmail.com>
2023-11-28 15:12:08 -0700

    some systems interacting with some components

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     App::new()
         .add_systems(PreStartup, setup)
         .add_systems(Update, say_hello)
+        .add_systems(Update, say_job)
+        .add_systems(Update, people_with_jobs)
+        .add_systems(Update, people_without_jobs)
+        .add_systems(Update, jobs)
         .add_systems(Update, hello_world_system)
         .run();
     println!("Main says, 'Hello, world!' as it exits");
@@ -14,11 +18,20 @@ fn hello_world_system() {
 }
 
 fn setup(mut commands: Commands) {
+    commands.spawn((
+        Person {
+            name: "Mark".to_string(),
+        },
+        Employed { job: Job::Nerd },
+    ));
+    commands.spawn((
+        Person {
+            name: "Oscar".to_string(),
+        },
+        Employed { job: Job::HousePet },
+    ));
     commands.spawn(Person {
-        name: "Mark".to_string(),
-    });
-    commands.spawn(Person {
-        name: "Oscar".to_string(),
+        name: "Judy".to_string(),
     });
 }
 
@@ -28,7 +41,44 @@ fn say_hello(query: Query<&Person>) {
     }
 }
 
+fn people_with_jobs(query: Query<&Person, With<Employed>>) {
+    for per in &query {
+        println!("Hello {}, you have a job", per.name);
+    }
+}
+
+fn people_without_jobs(query: Query<&Person, Without<Employed>>) {
+    for per in &query {
+        println!("Hello {}, you don't have a job", per.name);
+    }
+}
+
+fn jobs(query: Query<(&Person, &Employed)>) {
+    for (per, emp) in &query {
+        println!("{} is a {:?}", per.name, emp.job);
+    }
+}
+
+fn say_job(query: Query<&Employed>) {
+    for emp in &query {
+        println!("this person is a {:?}!", emp.job);
+    }
+}
+
 #[derive(Component)]
 struct Person {
     pub name: String,
+}
+
+#[derive(Debug)]
+enum Job {
+    Nerd,
+    //Docter,
+    //Teacher,
+    HousePet,
+}
+
+#[derive(Component)]
+struct Employed {
+    pub job: Job,
 }
